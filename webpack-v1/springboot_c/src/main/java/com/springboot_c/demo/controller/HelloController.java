2bfc06449c8e0fe12683a9fe63cae0325e0f68bf
lyy <183938622@qq.com>
2020-06-06 19:18:07 +0800

    springboot+swagger

---
+++ webpack-v1/springboot_c/src/main/java/com/springboot_c/demo/controller/HelloController.java
@@ -0,0 +1,23 @@
+package com.springboot_c.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author 鲁先生
+ * @create 2020-06-06 17:40
+ **/
+@RestController
+public class HelloController {
+
+  @RequestMapping("/hello")
+  public String   hello(){
+    return "hello";
+  }
+
+
+
+
+}
